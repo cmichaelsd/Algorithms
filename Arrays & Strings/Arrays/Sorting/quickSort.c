a4561eea3d15d973cf41b8bad9e0bf2c82991dce
Cole Michaels <cmichaelsd@gmail.com>
2020-10-29 16:44:09 -0700

    set up a quick sort impl.

--- Arrays & Strings/Arrays/Sorting/quickSort.c
+++ Arrays & Strings/Arrays/Sorting/quickSort.c
@@ -26,7 +26,12 @@ int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
 
+    printf("Hit\n");
     for (int j = low; j < high; ++j) {
+        /**
+         * Could improve by adding a condition 
+         * for work not to be done if i == j
+         */
         if (arr[j] < pivot) {
             ++i;
             swap(&arr[i], &arr[j]);
@@ -38,6 +43,11 @@ int partition(int arr[], int low, int high) {
     return i + 1;
 }
 
+/**
+ * O(n * log(n))
+ * Will run log(n) times where n = arrays size
+ * and for each call to partition a linear operation happens
+ */
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
@@ -47,12 +57,12 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
-int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int arr_count = sizeof(arr) / sizeof(int);
+// int main() {
+//     int arr[] = {10, 7, 8, 9, 1, 5};
+//     int arr_count = sizeof(arr) / sizeof(int);
 
-    quickSort(arr, 0, arr_count - 1);
-    printArray(arr, arr_count);
+//     quickSort(arr, 0, arr_count - 1);
+//     printArray(arr, arr_count);
 
-    return 0;
-}
+//     return 0;
+// }
