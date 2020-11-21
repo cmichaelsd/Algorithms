2b2a6358fa88fe0adfe03872bcf784a91d4230e0
Cole Michaels <cmichaelsd@gmail.com>
2020-11-21 14:55:53 -0800

    updated documentation for zeroMatrix.

--- Arrays & Strings/Arrays/zeroMatrix.c
+++ Arrays & Strings/Arrays/zeroMatrix.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include "helpers.h"
 
+
+/**
+ * O(m^v*m * n^v*n)
+ * 
+ * m = Number of rows
+ * n = Number of columns
+ * v = number of zeros in matrix
+ * 
+ * For each row                                          -> m *
+ *   For each column                                     -> n *
+ *     If the item is a 0                                -> v +
+ *       Replace all row items with 0 at current column  -> m
+ *       Replace all column items with 0 at current row  -> n
+ */
 void zeroMatrix(int matrix[MATRIX_M][MATRIX_N], int m_count, int n_count) {
     int indiciesMatrix[m_count][n_count];
     matrixMxNFill(indiciesMatrix, m_count, n_count, 1);
 
-    /* Map found zeros to indicies matrix */
+    /* Map found zeros to indicies matrix. */
     for (int row = 0; row < m_count; ++row) {
         for (int column = 0; column < n_count; ++column) {
             if (matrix[row][column] == 0) {
@@ -22,11 +36,12 @@ void zeroMatrix(int matrix[MATRIX_M][MATRIX_N], int m_count, int n_count) {
     for (int row = 0; row < m_count; ++row) {
         for (int column = 0; column < n_count; ++column) {
             if (indiciesMatrix[row][column] == 0) {
-                for (int i = 0; i < n_count; ++i) {
-                    matrix[row][i] = 0;
-                }
                 for (int j = 0; j < m_count; ++j) {
                     matrix[j][column] = 0;
+                }
+
+                for (int i = 0; i < n_count; ++i) {
+                    matrix[row][i] = 0;
                 }
             }
         }
