7f0590478be77dc3ca57a0012a8d0740012d9e53
Cole Michaels <cmichaelsd@gmail.com>
2021-01-14 12:16:02 +0900

    setting up stacks and queues.

---
+++ Stacks & Queues/stack.h
@@ -0,0 +1,74 @@
+#ifndef STACK
+#define STACK
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node {
+	int data;
+	struct node* next;
+} node_t;
+
+/**
+ * Time: O(c) 
+ * Space: O(c) 
+ * Pops off the top element of a stack given a top double pointer. 
+ * 
+ * @param node_t** top
+ * @return int
+ */
+int pop(node_t** top) {
+	if (*top == NULL) {
+		return -1;
+	}
+
+	int temp = *top->data;
+	*top = *top->next;
+
+	return temp;
+}
+
+/**
+ * Time: O(c) 
+ * Space: O(c) 
+ * Pushes a new node to a stack given a top double pointer. 
+ * 
+ * @param node_t** top 
+ * @param int data
+ */
+void push(node_t** top, int data) {
+	node_t* nTop = (node_t*) malloc(sizeof(node_t));
+
+	nTop->data = data;
+
+	*top->next = nTop;
+	*top = nTop;
+}
+
+/**
+ * Time: O(c) 
+ * Space: O(c) 
+ * Peeks the top elements data in a stack given a top pointer. 
+ * 
+ * @param node_t* top
+ * @return int
+ */
+int peek(node_t* top) {
+	if (top == NULL) {
+		return -1;
+	}
+
+	return top->data;
+}
+
+/**
+ * Time: (c) 
+ * Space: (c) 
+ * Returns 0 if a stack is empty and a 1 if it is not. 
+ * 
+ * @param node_t* top
+ */
+int isEmpty(node_t* top) {
+	return top == NULL;
+}
+
+#endif
---
+++ Stacks & Queues/threeInOne.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "stack.h"
+
+/**
+ * Describe how you could use a single array to implement three stacks.
+ */
+int main() {
+	node_t* top = NULL;
+	push(&top, 1);
+
+	return 0;
+}
